31d612938f23a84eb66f47038a16aa43720df983
IzumiShaka-desu <sesaka.aji19137@student.unsika.ac.id>
2021-05-31 10:32:22 +0700

    add color palette

---
+++ futbook/lib/constant/color_palette.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/painting.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+abstract class ColorPallete {
+  static final Color primary = HexColor("1E57F1");
+  static final Color altPrimary = HexColor("2E73B4");
+  static final Color altBlue = HexColor("F6F9FF");
+  static final Color secondary = HexColor("C4C4C4");
+  static final Color backgroundColor = HexColor("FFFFFF");
+  static final Color altBackgroundColor = HexColor("F9F9F9");
+  static final Color activeTextColor = HexColor("00C2FF");
+}
