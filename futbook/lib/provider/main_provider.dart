4c3fd1357e3f10cf100735c8d8de403a888432a1
IzumiShaka-desu <shakaaji29@gmail.com>
2021-06-04 22:53:08 +0700

    update  main provider

--- futbook/lib/provider/main_provider.dart
+++ futbook/lib/provider/main_provider.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:futbook/data/model/futsal.dart';
 
 class MainProvider extends ChangeNotifier {
   MainProvider._internal();
   static final _singleton = MainProvider._internal();
   factory MainProvider() => _singleton;
+  List<Futsal> _futsal = [];
+  List<Futsal> get futsal => _futsal;
+  void loadFutsalList() {}
 }
